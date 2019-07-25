57f9578b672ec2d026f7a4b8ef225d85eb7debcb
Hariet <harietlewis@gmail.com>
2019-07-24 20:40:43 -0400

    React Mockup Component

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+class Button extends Component {
+   constructor(props, ref) {
+    super(props, ref)
+    this.state = {
+        name: props.name || "Machu MiniGoldenDoodle",
+        counter: 0
+    }
+   }
+}
+
+handleOnClick() {
+    alert(`AYEEEEEE`);
+    this.setState({
+        counter: this.state.counter +1
+    })
+}
+
+// class Seperator extends Component {
+
+//     render() {
+
+//     }
+// }
+
 class App extends Component {
 
     constructor(props, ref) {
@@ -6,18 +30,22 @@ class App extends Component {
 
     handleOnClick() {
         console.log('test');
+        this.update();
     }
-  
+
     render() {
+        console.debug('in app render');
         return {
             tag: 'div',
             body: 'Test',
-            // onclick: this.handleOnClick.bind(this)
-            onclick: console.log('test')
+            onclick: this.handleOnClick.bind(this)
+            //onclick: console.log('test')
+            children: [
+
+            ]
         };
     }
 }
 
 const app = new App({}, 'app');
 app.update();
-  
